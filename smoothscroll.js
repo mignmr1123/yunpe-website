6d08d95ef539f01821e8f02f290fe850f32b6de5
minami okamura <mnmokmr1@gmail.com>
2022-07-31 15:00:18 +0900

    add files

---
+++ smoothscroll.js
@@ -0,0 +1,47 @@
+//aタグをクリックしたときのスムーズスクロール
+$(function(){
+ $('a[href^="#"]').click(function(){     // #で始まるアンカーをクリックした場合に処理
+  var speed = 1000;                    // スクロールの速度（1000＝１秒）
+  var href= $(this).attr("href");    // アンカーの値取得
+  var target = $(href == "#" || href == "" ? 'html' : href); // 移動先を取得
+  var position = target.offset().top;   // 移動先を数値で取得
+  $("html, body").animate({scrollTop:position}, speed, "swing"); // スムーススクロール
+  return false;
+ });
+});
+
+
+
+//一定の場所にきたらフェードインして表示されるボタンのスムーズスクロール
+$(function() {
+ var topBtn = $('.top-btn');	
+ topBtn.hide();
+ //スクロールが500に達したらボタン表示
+ $(window).scroll(function () {
+  if ($(this).scrollTop() > 250) {
+   topBtn.fadeIn();
+  } else {
+   topBtn.fadeOut();
+  }
+ });
+ //スクロールしてトップ
+ topBtn.click(function () {
+  $('body,html').animate({
+   scrollTop: 0
+  }, 250);
+  return false;
+ });
+});
+
+$(function() {
+  var bsiteBtn = $('#b-site');	
+  bsiteBtn.hide();
+  //スクロールが250に達したらボタン表示
+  $(window).scroll(function () {
+   if ($(this).scrollTop() > 250) {
+    bsiteBtn.fadeIn();
+   } else {
+    bsiteBtn.fadeOut();
+   }
+  });
+});
